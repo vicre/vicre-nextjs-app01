eeb9da6ced7497afa2e7eb09c4147e2bff1ff994
Victor Reipur <vicre@dtu.dk>
2024-11-26 13:32:53 +0000

    change to commonjs

--- app-main/next.config.js
+++ app-main/next.config.js
@@ -1,21 +1,13 @@
-// next.config.js
-
 const isLocal = process.env.NODE_ENV === 'development';
 
 export default {
   images: {
-    domains: [],
+    domains: ['firebasestorage.googleapis.com'],
   },
   webpack(config, { dev }) {
     if (dev) {
       config.devtool = 'cheap-module-source-map';
     }
     return config;
-  },
-  ...(isLocal
-    ? {} // Local environment: no additional configuration
-    : {
-        functions: {
-        },
-      }),
-};
+  }
+}
