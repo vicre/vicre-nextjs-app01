a8c6c669a3ce5699ad6ad2a714a1b6c6eab3689e
Victor Reipur <vicre@dtu.dk>
2024-11-26 12:57:13 +0000

    nu virker tailwind

--- app-main/pages/hello-world.js
+++ app-main/pages/hello-world.js
@@ -1,8 +1,7 @@
 export default function HelloWorld() {
     return (
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        <h1 className="text-4xl font-bold text-blue-500">Hello World!</h1>
-      </div>
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold text-blue-500">Hello World!</h1>
+        </div>
     );
-  }
-  
+}
--- app-main/tailwind.config.js
+++ app-main/tailwind.config.js
@@ -2,12 +2,4 @@ export const content = [
   "./pages/**/*.{js,ts,jsx,tsx}",
   "./components/**/*.{js,ts,jsx,tsx}", // Include this if you have a components folder
 ];
-export const plugins = [
-  require('@tailwindcss/aspect-ratio'),
-];
-export const extend = {
-  animation: {
-    'spin-slow': 'circle 6s linear infinite', // Adjust timing as needed
-  }
-};
-  
+
