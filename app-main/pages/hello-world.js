f6a6c104baf9a136f1c34071aa1ebabc19bba0c3
Victor Reipur <vicre@dtu.dk>
2024-11-26 09:48:08 +0000

    third commit

---
+++ app-main/pages/hello-world.js
@@ -0,0 +1,8 @@
+export default function HelloWorld() {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-500">Hello World!</h1>
+      </div>
+    );
+  }
+  
