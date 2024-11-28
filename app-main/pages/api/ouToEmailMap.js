a8872410e35a6679793b55b58108069056d1d534
Victor Reipur <vicre@dtu.dk>
2024-11-28 15:37:25 +0000

    put in production

--- app-main/pages/api/ouToEmailMap.js
+++ app-main/pages/api/ouToEmailMap.js
@@ -1,22 +1,34 @@
+// pages/api/ouToEmailMap.js
+
 export default function handler(req, res) {
+    // Get the secret key from environment variables
+    const secretKey = process.env.SECRET_KEY;
+
+    // Get the provided key from headers or query parameters
+    const providedKey = req.headers['x-api-key'] || req.query.api_key;
+
+    // Check if the provided key matches the secret key
+    if (providedKey !== secretKey) {
+        return res.status(401).json({ error: 'Unauthorized: Invalid API Key' });
+    }
+
     // Define the Organizational Unit (OU) to Email mapping
     const ouToEmailMap = [
         {
-          targetOUs: [
-            "OU=Dummy1,OU=BaseUsers,DC=example,DC=com",
-            "OU=Dummy2,OU=BaseUsers,DC=example,DC=com"
-          ],
-          email: "dummy1@example.com"
+            targetOUs: [
+                "OU=Dummy1,OU=BaseUsers,DC=example,DC=com",
+                "OU=Dummy2,OU=BaseUsers,DC=example,DC=com"
+            ],
+            email: "dummy1@example.com"
         },
         {
-          targetOUs: [
-            "OU=Dummy3,OU=BaseUsers,DC=example,DC=com"
-          ],
-          email: "dummy2@example.com"
+            targetOUs: [
+                "OU=Dummy3,OU=BaseUsers,DC=example,DC=com"
+            ],
+            email: "dummy2@example.com"
         }
-      ];
-  
+    ];
+
     // Return the OU to Email map as JSON
     return res.status(200).json({ ouToEmailMap });
-  }
-  
+}
