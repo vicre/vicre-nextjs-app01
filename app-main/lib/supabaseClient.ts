30b702595889a8b4e67fcc96c1108698a46ee3d8
Victor <vicre@dtu.dk>
2025-02-12 12:10:04 +0000

    commit fæør arbejde

--- app-main/lib/supabaseClient.ts
+++ app-main/lib/supabaseClient.ts
@@ -4,6 +4,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // These env vars are prefixed with NEXT_PUBLIC_, so they're exposed to the browser:
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON!
 
 export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey)
